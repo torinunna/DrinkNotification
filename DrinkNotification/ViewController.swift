2f8c550866f6eef8d5c2dead14cc5a02ce977fea
torinunna <torinunna9588@gmail.com>
2023-09-01 23:42:07 +0900

    Initial Commit

---
+++ DrinkNotification/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DrinkNotification
+//
+//  Created by YUJIN KWON on 2023/09/01.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
