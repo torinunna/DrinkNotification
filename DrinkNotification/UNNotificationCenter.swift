c267a008466b5e0b10796564cbadb64454332d79
torinunna <torinunna9588@gmail.com>
2023-09-02 16:05:26 +0900

    set trigger

--- DrinkNotification/UNNotificationCenter.swift
+++ DrinkNotification/UNNotificationCenter.swift
@@ -15,5 +15,8 @@ extension UNUserNotificationCenter {
         content.body = "세계보건기구(WHO)가 권장하는 하루 물 섭취량은 1.5~2리터 입니다."
         content.sound = .default
         content.badge = 1
+        
+        let component = Calendar.current.dateComponents([.hour, .minute], from: alert.date)
+        let trigger = UNCalendarNotificationTrigger(dateMatching: component, repeats: alert.isOn)
     }
 }
