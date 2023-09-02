d959c801aa3ac663f04094fa0d7c2af2522e1baf
torinunna <torinunna9588@gmail.com>
2023-09-02 16:02:10 +0900

    set UserNotification content

--- DrinkNotification/AppDelegate.swift
+++ DrinkNotification/AppDelegate.swift
@@ -6,14 +6,24 @@
 //
 
 import UIKit
+import NotificationCenter
+import UserNotifications
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
+    var userNotificationCenter: UNUserNotificationCenter?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        UNUserNotificationCenter.current().delegate = self
+        
+        let authorizationOptions = UNAuthorizationOptions(arrayLiteral: [.alert, .badge, .sound])
+        userNotificationCenter?.requestAuthorization(options: authorizationOptions) { _, error in
+            if let error = error {
+                print("Error: notification authorization request \(error.localizedDescription)")
+            }
+        }
+    
         return true
     }
 
@@ -34,3 +44,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 }
 
+extension AppDelegate: UNUserNotificationCenterDelegate {
+    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+        completionHandler([.banner, .list, .badge, .sound])
+    }
+    
+    func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: @escaping () -> Void) {
+        completionHandler()
+    }
+}
---
+++ DrinkNotification/UNNotificationCenter.swift
@@ -0,0 +1,19 @@
+//
+//  UNNotificationCenter.swift
+//  DrinkNotification
+//
+//  Created by YUJIN KWON on 2023/09/02.
+//
+
+import Foundation
+import UserNotifications
+
+extension UNUserNotificationCenter {
+    func addNotificationRequest(by alert: Alert) {
+        let content = UNMutableNotificationContent()
+        content.title = "물 마실 시간입니다"
+        content.body = "세계보건기구(WHO)가 권장하는 하루 물 섭취량은 1.5~2리터 입니다."
+        content.sound = .default
+        content.badge = 1
+    }
+}
